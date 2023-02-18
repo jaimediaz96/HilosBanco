ec8cc21e99a1e29ce20da3a0cbe3f587468b1c09
JaimeDiaz <jedt1996@gmail.com>
2023-02-17 22:09:25 -0500

    Se soluciona error de los hilos que mueren sin hacer transaccion

--- src/hilosbanco/Banco.java
+++ src/hilosbanco/Banco.java
@@ -1,5 +1,6 @@
 package hilosbanco;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -11,25 +12,28 @@ public class Banco {
 
     private final double cuentas[];
     private Lock cierreBanco = new ReentrantLock();
+    private Condition saldoSuficiente;
 
     public Banco(int salarioInicial) {
         cuentas = new double[100];
         for (int i = 0; i < cuentas.length; i++) {
             cuentas[i] = salarioInicial;
         }
+        saldoSuficiente = cierreBanco.newCondition();
     }
 
-    public void transferencia(int cuentaOrigen, int cuentaDestino, double cantidad) {
+    public void transferencia(int cuentaOrigen, int cuentaDestino, double cantidad) throws InterruptedException {
         cierreBanco.lock();
         try {
-            if (cuentas[cuentaOrigen] < cantidad) { // evalua que el saldo no es inferior a la transferencia
-                return;
+            while(cuentas[cuentaOrigen] < cantidad) { // evalua que el saldo no es inferior a la transferencia
+                saldoSuficiente.await();
             }
             System.out.println(Thread.currentThread());
             cuentas[cuentaOrigen] -= cantidad; //dinero que sale de la cuenta
             System.out.printf("%10.2f de %d para %d", cantidad, cuentaOrigen, cuentaDestino);
             cuentas[cuentaDestino] += cantidad;
             System.out.printf(" Saldo total: %10.2f%n", getSaldoTotal());
+            saldoSuficiente.signalAll();
         } finally {
             cierreBanco.unlock();
         }
