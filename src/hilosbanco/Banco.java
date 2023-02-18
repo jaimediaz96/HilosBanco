ecf3be1228129554c113475242df1e0d4a806abe
JaimeDiaz <jedt1996@gmail.com>
2023-02-17 22:17:32 -0500

    Cierre o bloque implicito

--- src/hilosbanco/Banco.java
+++ src/hilosbanco/Banco.java
@@ -11,32 +11,24 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Banco {
 
     private final double cuentas[];
-    private Lock cierreBanco = new ReentrantLock();
-    private Condition saldoSuficiente;
 
     public Banco(int salarioInicial) {
         cuentas = new double[100];
         for (int i = 0; i < cuentas.length; i++) {
             cuentas[i] = salarioInicial;
         }
-        saldoSuficiente = cierreBanco.newCondition();
     }
 
-    public void transferencia(int cuentaOrigen, int cuentaDestino, double cantidad) throws InterruptedException {
-        cierreBanco.lock();
-        try {
-            while(cuentas[cuentaOrigen] < cantidad) { // evalua que el saldo no es inferior a la transferencia
-                saldoSuficiente.await();
-            }
-            System.out.println(Thread.currentThread());
-            cuentas[cuentaOrigen] -= cantidad; //dinero que sale de la cuenta
-            System.out.printf("%10.2f de %d para %d", cantidad, cuentaOrigen, cuentaDestino);
-            cuentas[cuentaDestino] += cantidad;
-            System.out.printf(" Saldo total: %10.2f%n", getSaldoTotal());
-            saldoSuficiente.signalAll();
-        } finally {
-            cierreBanco.unlock();
-        }
+    public synchronized void transferencia(int cuentaOrigen, int cuentaDestino, double cantidad) throws InterruptedException {
+        while (cuentas[cuentaOrigen] < cantidad) { // evalua que el saldo no es inferior a la transferencia
+            wait(); // pone el hilo a la espera hasta que se cumpla la condicion y libera el codigo para que otro hilo pueda entrar
+        } // de la forma implicita solo se puede generar una condicion, de la forma explicita se pueden poner tantas condiciones sean necesarias
+        System.out.println(Thread.currentThread());
+        cuentas[cuentaOrigen] -= cantidad; //dinero que sale de la cuenta
+        System.out.printf("%10.2f de %d para %d", cantidad, cuentaOrigen, cuentaDestino);
+        cuentas[cuentaDestino] += cantidad;
+        System.out.printf(" Saldo total: %10.2f%n", getSaldoTotal());
+        notifyAll(); // Notifica o despierta a los hilos en espera para que intenten de nuevo su condicion
     }
 
     public double getSaldoTotal() {
